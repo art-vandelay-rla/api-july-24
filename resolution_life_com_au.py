016d3f617f5d9456c8d54a5d19f6e3d8ff25ef26
art-vandelay-rla <art.vandelay.rla@gmail.com>
2024-07-18 10:47:57 +1000

    Create resolution_life_com_au.py

---
+++ resolution_life_com_au.py
@@ -0,0 +1,17 @@
+import requests
+
+# Credentials for the API
+auth = requests.auth.HTTPBasicAuth('philippa.hawk@resolutionlife.co.nz','fecf6e30-449e-11ef-b3fc-7a67b0c07e2e')
+
+def main() -> int:
+  # API endpoint
+  url = 'https://resolutionlife.com.au/'
+  
+  # Retrieve data
+  response = requests.get(url, auth=auth)
+  
+  print(response.text)
+  return 0
+
+if __name__ == '__main__':
+  sys.exit(main())
